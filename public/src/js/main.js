8fac5a8090ba91d90e7b49f95d40fb0351edaee9
April Polubiec <aprilpolubiec@gmail.com>
2020-05-26 22:38:33 -0500

    add auth and firestore docs

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -2,41 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
   const db = firebase.firestore()
   const auth = firebase.auth()
   const functions = firebase.functions()
+
   if (window.location.hostname == 'localhost') {
     functions.useFunctionsEmulator('http://localhost:5001')
   }
 
-  const displayRandomBook = (swipe_direction) => {
-    const findBook = functions.httpsCallable('findBook')
-    findBook().then((result) => {
-      var bookInfo = result.data.volumeInfo
-      var bookCard = $(`<div class='book-card center'></div>`)
-      // $('#book-container .book-card:nth-last-child(2)').removeClass('left')
-      // bookContainer.children().last().removeClass('center').addClass('left')
-      bookContainer.children().last().remove()
-      bookContainer.append(bookCard)
-      bookCard.append(`<img src='${bookInfo.imageLinks.thumbnail}'></img>`)
-      bookCard.append(
-        `<div>Title: ${bookInfo.title}${
-          bookInfo.subtitle ? `: ${bookInfo.subtitle}` : ''
-        }</div>`
-      )
-      bookCard.append(`<div>Author: ${bookInfo.authors[0]}</div>`)
-      bookCard.append(`<div>Description: ${bookInfo.description}</div>`)
-      bookCard.append(
-        `<a href="${bookInfo.previewLink}" target="_blank">Preview</a>`
-      )
-
-      //Add click event listener to book card
-      // bookCard.click((e) => {
-      //   console.log(e)
-      //   bookCard.next().removeClass('center').addClass('right')
-      //   bookCard.removeClass('left').addClass('center')
-      //   bookCard.previous().addClass('left')
-      // })
-    })
-  }
-
+  const main = $('#main')
   const findBookButton = $('#find-book')
   const bookContainer = $('#book-container')
 
@@ -48,15 +19,99 @@ document.addEventListener('DOMContentLoaded', function () {
     `<button type="button"><i class="fas fa-check-circle"></i></button>`
   )
   likeButtons.append(dislikeButton, likeButton)
+  var currentBook = {}
+
+  //Firebase auth UI
+  var ui = new firebaseui.auth.AuthUI(auth)
+  var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        console.log(authResult, redirectUrl)
+        var { email, name, picture } = authResult.additionalUserInfo.profile
+        if (authResult.additionalUserInfo.isNewUser) {
+          //Create doc
+          db.collection('users').doc(authResult.user.uid).set({
+            email,
+            name,
+            picture,
+          })
+        }
+        $('#login').css('visibility', 'hidden')
+        $('#mask').remove()
+        displayRandomBook()
+        return false
+      },
+      uiShown: function () {
+        document.getElementById('loader').style.display = 'none'
+      },
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+  }
+
+  ui.start('#firebaseui-auth-container', uiConfig)
+
+  const renderBook = (bookData) => {
+    var bookInfo = bookData.data.volumeInfo
+    var bookCard = $(`<div class='book-card center'></div>`)
+    bookContainer.children().last().remove()
+    bookContainer.append(bookCard)
+    bookCard.append(`<img src='${bookInfo.imageLinks.thumbnail}'></img>`)
+    bookCard.append(
+      `<div>Title: ${bookInfo.title}${
+        bookInfo.subtitle ? `: ${bookInfo.subtitle}` : ''
+      }</div>`
+    )
+    bookCard.append(`<div>Author: ${bookInfo.authors[0]}</div>`)
+    bookCard.append(`<div>Description: ${bookInfo.description}</div>`)
+    bookCard.append(
+      `<a href="${bookInfo.previewLink}" target="_blank">Preview</a>`
+    )
+  }
+
+  const renderLoginScreen = () => {
+    const pageMask = $(`<div id='mask'></div>`)
+    const login = $('#login')
+    main.append(pageMask)
+    login.css('visibility', 'visible')
+  }
+
+  const displayRandomBook = () => {
+    const findBook = functions.httpsCallable('findBook')
+    findBook().then((result) => {
+      currentBook = result
+      renderBook(result)
+    })
+  }
+
+  const updateInterests = () => {
+    console.log(currentBook)
+    //db.collection('users').doc(auth.currentUser.uid).update()
+  }
 
   likeButton.click(() => {
     $('.book-card').removeClass('center').addClass('swipe').addClass('right')
-    displayRandomBook()
+    if (auth.currentUser) {
+      updateInterests()
+      displayRandomBook()
+    } else {
+      renderLoginScreen()
+    }
   })
 
   dislikeButton.click(() => {
     $('.book-card').removeClass('center').addClass('swipe').addClass('left')
-    displayRandomBook()
+    if (auth.currentUser) {
+      updateInterests()
+      displayRandomBook()
+    } else {
+      renderLoginScreen()
+    }
   })
 
   findBookButton.click(() => {
