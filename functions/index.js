45f44ac1cf00d8404dc4b92b332894ecd447d505
April Polubiec <aprilpolubiec@gmail.com>
2020-05-30 14:12:24 -0500

    update functions and use axios

--- functions/index.js
+++ functions/index.js
@@ -1,25 +1,29 @@
+const admin = require('firebase-admin')
 const functions = require('firebase-functions')
-const fetch = require('node-fetch')
+const axios = require('axios')
 const randomWords = require('random-words')
 
+admin.initializeApp()
+
 exports.findBook = functions.https.onCall((data, context) => {
-  console.log(`data: ${data}\ncontext: ${context}`)
   const randomWord = randomWords()
   console.log(randomWord)
-  return fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=insubject:${randomWord}&key=${
-      functions.config().books.api.key
-    }`
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      const { totalItems } = json
+  return axios
+    .get(
+      `https://www.googleapis.com/books/v1/volumes?country=US&q=insubject:${randomWord}&key=${
+        functions.config().books.api.key
+      }`
+    )
+    .then((response) => {
+      var data = response.data
+      const { totalItems } = data
       var randomNumber
       if (totalItems < 10) {
         randomNumber = Math.floor(Math.random() * totalItems)
       } else {
         randomNumber = Math.floor(Math.random() * 9)
       }
-      return json.items[randomNumber]
+      return data.items[randomNumber]
     })
+    .catch((error) => console.log(error))
 })
